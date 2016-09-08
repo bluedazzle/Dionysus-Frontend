4c437864532909f88635595a965f33fb924b5fb1
rapospectre <rapospectre@163.com>
2016-09-08 12:26:10 +0800

    <feat>  init project
    Author:rapospectre@gmail.com
    
    Desc: init project

---
+++ src/js/index/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+let App = React.createClass({
+    getInitialState: function(){
+        return {title: "Dionysus"}
+    },
+
+    render: function(){
+        return <div><h1>{this.state.title}</h1></div>
+    }
+})
+
+ReactDOM.render(<App/>, document.getElementById('AppRoot'));
